6948980780aed8bd69ff5bc4cfc5e635b09ff728
Beytullahzor7 <beytullahzor7@gmail.com>
2022-04-10 16:23:09 +0300

    Day25 Updated

--- src/HackerRank/IsPrimeHackerRankDay25.java
+++ src/HackerRank/IsPrimeHackerRankDay25.java
@@ -19,7 +19,7 @@ public class IsPrimeHackerRankDay25 {
     public static boolean isPrime(int num){
         if(num == 1) return false;
 
-        for(int i=2; i<num; i++){
+        for(int i=2; i*i<=num; i++){ // O(√¯n)
             if(num % i == 0) return false;
         }
         return true;
